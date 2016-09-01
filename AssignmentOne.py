eff9cfab0eb7397b9d3f8425bbe66b8867e1f211
Braden <browl1742@gmail.com>
2016-09-01 20:43:33 +1000

    Trying some code to refine down the list into parts to be broken apart

--- AssignmentOne.py
+++ AssignmentOne.py
@@ -5,8 +5,16 @@ def menuChoice():
     while "CRQ" not in useChoice:
         useChoice = input("Please enter a choice between list of completed items, list of required items or to quit")
 
-def loadFile():
-    itemsList = []
-    listFile = open('items', 'w')
-    for data in listFile:
-        itemsList.append(data)
+#def loadFile():
+itemsList = []
+listFile = open('items.csv', 'r')
+for data in listFile:
+    itemsList.append(data)
+ #   print(data)
+
+
+secondList= []
+for i in itemsList:
+    newText = i.split(",")
+    secondList.append(newText)
+print(secondList)
