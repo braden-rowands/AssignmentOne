1c9ec887aa1f06cb512b4ee99e2220a35b2e4b8f
Braden <browl1742@gmail.com>
2016-09-02 15:19:16 +1000

    Cleaned up some code and fixed the menu

--- AssignmentOne.py
+++ AssignmentOne.py
@@ -2,15 +2,20 @@ userName = input("Please enter your name")
 
 def menuChoice():
     useChoice = input("Please enter a choice between list of completed items, list of required items or to quit")
-    while "CRQ" not in useChoice:
-        useChoice = input("Please enter a choice between list of completed items, list of required items or to quit")
+    while useChoice != "Q":
+        if useChoice == "R":
+            print ("This will show required tasks")
+        elif useChoice == "C":
+            print("This will show completed tasks")
+        else:
+            useChoice = input("Please enter a valid choice between Q,R or C")
 
-#def loadFile():
-itemsList = []
-listFile = open('items.csv', 'r')
-for data in listFile:
+def loadFile():
+    itemsList = []
+    listFile = open('items.csv', 'r')
+    for data in listFile:
     itemsList.append(data)
- #   print(data)
+        #   print(data)
 
 
 secondList= []
